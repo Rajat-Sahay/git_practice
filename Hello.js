30c93f780ba45d77098c104d5e7d064b40f5bde6
Rajat Sahay <sahay.r@northeastern.edu>
2025-07-28 18:26:12 -0400

    Just did Git stash to save prod

--- Hello.js
+++ Hello.js
@@ -1,8 +1,3 @@
 console.log('Rajat Sahay')
-<<<<<<< HEAD
 console.log('Hello, Git')
-console.log('Hello from Dev rajat')
-console.log('TrYING git revert')
-=======
-console.log('Hello, Git')
->>>>>>> parent of 4d0a8d1 (Added third console log in Hello.js File)
+//bug fix
