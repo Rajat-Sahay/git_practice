4178af2895fb07293624f81fc37b87b5d67d4119
Rajat Sahay <sahay.r@northeastern.edu>
2025-07-28 18:21:38 -0400

    Revert "Added third console log in Hello.js File"
    This reverts commit 4d0a8d186391c619cef78b75617211b9a4d4bbf1.

--- Hello.js
+++ Hello.js
@@ -1,4 +1,8 @@
 console.log('Rajat Sahay')
+<<<<<<< HEAD
 console.log('Hello, Git')
 console.log('Hello from Dev rajat')
 console.log('TrYING git revert')
+=======
+console.log('Hello, Git')
+>>>>>>> parent of 4d0a8d1 (Added third console log in Hello.js File)
